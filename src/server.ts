649c5055aa7387b6020db9e9d0f84c91099dbaf2
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-16 13:22:14 +0600

    updated project

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,29 @@
-import express from 'express';
-const app = express()
+import express, { 
+    type Application, 
+    type Request, 
+    type Response } from 'express';
+
+const app:Application = express()
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Express with TypeScript!')
-})
+app.use(express.json());
+
+app.get('/', (req:Request, res :Response) => {
+//   res.send('Express with TypeScript!')
+res.status(200).json({
+    message: 'express server!',
+    "author": "Programming Hero"
+  });
+
+});
+app.post('/',async (req:Request, res :Response) => {
+console.log(req.body);
+//   res.status(201).json({
+//     success: true,
+//     data: req.body
+//   });
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
