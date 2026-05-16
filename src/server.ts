b1339a82f6cb8d0f03bd6da974330354d04ed0a6
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-16 22:47:08 +0600

    created express server

--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,30 @@ app.use(express.json());
 app.use (express.text());
 app.use(express.urlencoded({extended: true}));
 
-const client = new Pool({
+const pool = new Pool({
     connectionString : "postgresql://neondb_owner:npg_QFTu6UPcrSE7@ep-tiny-mountain-ap8lzqs8-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-})
+});
+
+const initBD = async () => {
+    try {
+       await pool.query(` 
+        CREATE TABLe  IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) ,
+            email VARCHAR(50) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            is_active BOOLEAN DEFAULT true,
+            age INT,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        ) 
+        `);
+        console.log('table created successfully');
+    } catch (error) {
+        console.log(error);
+    }
+}; 
+initBD();
 
 
 app.get('/', (req:Request, res :Response) => {
