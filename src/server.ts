dce50e59613b85850d170859ca6fa238e50498dc
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-16 13:52:04 +0600

    updated project

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,19 @@ import express, {
     type Response } from 'express';
 
 const app:Application = express()
+import {Pool} from "pg"
+
+
 const port = 5000;
 
 app.use(express.json());
+app.use (express.text());
+app.use(express.urlencoded({extended: true}));
+
+const client = new Pool({
+    connectionString : "postgresql://neondb_owner:npg_QFTu6UPcrSE7@ep-tiny-mountain-ap8lzqs8-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
 
 app.get('/', (req:Request, res :Response) => {
 //   res.send('Express with TypeScript!')
@@ -17,12 +27,15 @@ res.status(200).json({
 
 });
 app.post('/',async (req:Request, res :Response) => {
-console.log(req.body);
-//   res.status(201).json({
-//     success: true,
-//     data: req.body
-//   });
-
+// console.log(req.body);
+ const {name, email, password} = req.body;
+  res.status(201).json({
+    message: 'Created successfully!',
+    data: {name,
+         email,
+        },
+      
+  })
 });
 
 app.listen(port, () => {
