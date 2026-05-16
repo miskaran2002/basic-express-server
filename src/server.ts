be76f505e0de43cf0ea3db07d2d5995193f72c57
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-16 23:45:11 +0600

    created express server

--- src/server.ts
+++ src/server.ts
@@ -49,14 +49,27 @@ res.status(200).json({
 });
 app.post('/',async (req:Request, res :Response) => {
 // console.log(req.body);
- const {name, email, password} = req.body;
+ const {name, email, password,age} = req.body;
+
+ try{
+    const result = await pool.query('INSERT INTO users (name, email, password, age) VALUES ($1, $2, $3, $4) RETURNING *', [name, email, password,age]);
+
+// console.log(result);
+
   res.status(201).json({
-    message: 'Created successfully!',
-    data: {name,
-         email,
-        },
+    message: ' user Created successfully!',
+    data: result.rows[0]
       
-  })
+  });
+ }catch (error: any) {
+    res.status(500).json({
+    message: error.message,
+    error: error
+  });
+ }
+ 
+
+
 });
 
 app.listen(port, () => {
