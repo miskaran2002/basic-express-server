47cf46f1efad9f2522cda18c9a45ded696696b10
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-17 12:37:33 +0600

    get a singal id

--- src/server.ts
+++ src/server.ts
@@ -47,7 +47,12 @@ res.status(200).json({
   });
 
 });
-app.post('/',async (req:Request, res :Response) => {
+
+
+
+
+
+app.post('/api/users',async (req:Request, res :Response) => {
 // console.log(req.body);
  const {name, email, password,age} = req.body;
 
@@ -57,12 +62,14 @@ app.post('/',async (req:Request, res :Response) => {
 // console.log(result);
 
   res.status(201).json({
+      success: true,
     message: ' user Created successfully!',
     data: result.rows[0]
       
   });
  }catch (error: any) {
     res.status(500).json({
+          success: false,
     message: error.message,
     error: error
   });
@@ -72,6 +79,62 @@ app.post('/',async (req:Request, res :Response) => {
 
 });
 
+
+app.get('/api/users', async (req:Request, res :Response) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.status(200).json({
+            success: true,
+            message: 'Users retrieved successfully!',
+            data: result.rows,
+        });
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+        message: error.message,
+        error: error
+      });
+     }
+    
+})
+
+
+
+app.get('/api/users/:id', async (req:Request, res :Response) => {
+    const { id } = req.params;
+    try{
+        const result = await pool.query('SELECT * FROM users WHERE id = $1',
+             [id]);
+
+
+
+        if (result.rows.length === 0) {
+             res.status(404).json({
+            success: false,
+            message: 'User not found!',
+            data: null,
+        })
+
+            
+        }
+
+
+        res.status(200).json({
+            success: true,
+            message: 'User retrieved successfully!',
+            data: result.rows[0],
+        })
+
+    }catch (error: any) {
+        res.status(500).json({
+            success: false,
+        message: error.message,
+        error: error
+      });
+     }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
+ 
