9bb2a920b0f4af3f75b9fc8c58ddd9181764be8d
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-17 13:22:46 +0600

    updated route create in server ts

--- src/server.ts
+++ src/server.ts
@@ -134,6 +134,41 @@ app.get('/api/users/:id', async (req:Request, res :Response) => {
      }
 })
 
+app.put('/api/users/:id', async (req:Request, res :Response) => {
+const { id } = req.params;
+    const { name, is_active, password, age } = req.body;
+
+
+
+    try {
+        const result = await pool.query('UPDATE users SET name = $1, password = $2, age = $3, is_active = $4, updated_at = NOW() WHERE id = $5 RETURNING *',
+     [name,  password, age,is_active, id]);
+
+     if (result.rows.length === 0) {
+        res.status(404).json({
+            success: false,
+            message: 'User not found!',
+            data: null,
+        })
+    };
+
+     res.status(200).json({
+        success: true,
+        message: 'User updated successfully',
+        data: result.rows[0]
+    });
+
+    }catch (error: any) {
+        res.status(500).json({
+            success: false,
+        message: error.message,
+        error: error
+      });
+     }
+
+    
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 }) 
