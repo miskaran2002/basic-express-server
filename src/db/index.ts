add5ed958e3a8dedc87d6e36226917c551f9ac6f
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-19 14:55:43 +0600

    update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -19,6 +19,28 @@ export const initDB = async () => {
             updated_at TIMESTAMP DEFAULT NOW()
         ) 
         `);
+
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS profiles(
+
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            bio TEXT,
+            address TEXT,
+            phone VARCHAR(20),
+            gender VARCHAR(10),
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+
+
+        )
+            
+            
+            
+            `);
+
+
+
         console.log('Database created successfully');
     } catch (error) {
         console.log(error);
