89602e28e7f626d00bb5c1cdaf1e571073dfb294
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-19 12:19:39 +0600

    impliment moduler pattern

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import express, {
 
 const app: Application = express();
 import {  pool } from './db';
+import { userRoute } from './modules/user/user.route';
 
 
 
@@ -25,36 +26,12 @@ app.get('/', (req: Request, res: Response) => {
 
 });
 
+app.use('/api/users', userRoute);
 
 
 
 
-app.post('/api/users', async (req: Request, res: Response) => {
-    // console.log(req.body);
-    const { name, email, password, age } = req.body;
 
-    try {
-        const result = await pool.query('INSERT INTO users (name, email, password, age) VALUES ($1, $2, $3, $4) RETURNING *', [name, email, password, age]);
-
-        // console.log(result);
-
-        res.status(201).json({
-            success: true,
-            message: ' user Created successfully!',
-            data: result.rows[0]
-
-        });
-    } catch (error: any) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-            error: error
-        });
-    }
-
-
-
-});
 
 
 app.get('/api/users', async (req: Request, res: Response) => {
---
+++ src/modules/user/user.interface.ts
@@ -0,0 +1,13 @@
+
+
+ export interface IUser {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    age: number;
+    is_active?: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+ }
+
