69fc802b3da030f58091c0d0cb12d27a044668bb
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-20 23:49:20 +0600

    created loger middleware

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const app: Application = express();
 import { userRoute } from './modules/user/user.route';
 import { profileRoute } from './modules/profile/profile.route';
 import { authRoute } from './modules/auth/auth.route';
+import fs from 'fs';
 
 
 
@@ -15,6 +16,21 @@ import { authRoute } from './modules/auth/auth.route';
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use((req, res, next) => {
+  console.log(' Method - URL  Time:',req.method, req.url, Date.now());
+
+  const log= `\nMethod - URL  Time: ${req.method} ${req.url} ${Date.now()}\n`;
+
+
+    fs.appendFile('logger.txt', log + '\n', (err) => {
+        console.log(err); 
+    });
+
+
+  next();
+});
 
 
 
